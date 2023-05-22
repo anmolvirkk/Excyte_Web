e827ce54789302e05805ef41f42ca8e05857e523
Anmol Virk <theanmolvirk@gmail.com>
2023-05-22 15:45:15 +1000

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/icon.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
